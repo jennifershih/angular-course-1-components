a6a4b1a563db69db06645f466420d3f9330df7d8
jennifershih <s01059810@gmail.com>
2021-08-09 21:58:07 +0800

    練習Pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,12 @@ import { Course } from './model/course';
 export class AppComponent {
   courses = COURSES;
 
+  startDate = new Date('2021/08/09');
+  title = COURSES[0].description;
+  price = 9.87;
+  rate = 0.76;
+  course = COURSES[0];
+
   OnCourseSelected(course:Course){
     console.log("App component",course);
   }
